768b0702d6aafdac3472b11ee00f9da15431aae0
Steve Vaught <Steven.D.Vaught@gmail.com>
2016-12-23 21:51:27 -0600

    create weasel.cxx with hello world

---
+++ weasel.cxx
@@ -0,0 +1,24 @@
+/*
+ * Steve Vaught
+ * This program uses the Unlicense
+ */
+
+// == Include statements ==
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+// == Function prototypes ==
+
+// == Main ==
+int main(){
+  cout << "Hello world!" << endl;
+
+
+
+  return 0;
+}
+
